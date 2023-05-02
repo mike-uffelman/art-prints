79dd04d497d11880beaf51ae37571f53aee9e032
mike-uffelman <muffelma@gmail.com>
2023-05-01 23:59:33 -0400

    updated image component to display full image without frame on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,18 +1,41 @@
 import './Image.css';
+import { useState, useRef } from 'react';
 import classNames from "classnames";
 
 export default function Image({product, modalOpen, className}) {
+    const imgRef = useRef();
+    const [ isHovered, setIsHovered ] = useState(null)
+    
     const classes = classNames(className)
-    console.log(classes)
+
+    const handleMouseOver = () => {
+        imgRef.current.classList.toggle('hovered')
+
+        setIsHovered(true)
+    }
+
+    const handleMouseOut = () => {
+        imgRef.current.classList.toggle('hovered')
+        setIsHovered(null)
+    }
+
+    const hovered = isHovered ? 'hovered' : '';
 
     return (
-        <div className={`img__container ${classes}`}>
-            <div className={`img__border ${classes}`}>
-                <div className={`img__inset ${classes}`}>
-                    <img onClick={modalOpen}  className={`img__product ${classes}`} src={product.image_urls.regular} alt={product.description}/>
-
-                </div>
-            </div>
+        <div className='img' >
+            <div className={`img__container ${classes} ${hovered}`}  onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            {/* {isHovered 
+                ?   
+                <img onClick={modalOpen}  className={`img__product`}  src={product.image_urls.regular} alt={product.description}/> 
+                :    */}
+                <div className={`img__border ${classes} ${hovered}`}>
+                        <div className={`img__inset ${classes} ${hovered}`}>
+                            <img onClick={modalOpen} ref={imgRef} className={`img__product ${classes} ${hovered}`} src={product.image_urls.regular} alt={product.description}/>
+                        </div>
+                    </div>
+            {/* } */}
         </div>
+        </div>
+        
     ) 
 }
