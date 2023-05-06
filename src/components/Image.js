c05758eb20f1a1940c8b9875db2e4c27142deebc
mike-uffelman <muffelma@gmail.com>
2023-05-05 23:57:30 -0400

    updated image layout and src switch by class name added

--- src/components/Image.js
+++ src/components/Image.js
@@ -19,22 +19,41 @@ export default function Image({product, modalOpen, className}) {
         setIsHovered(null)
     }
 
+    const imgSwitch = () => {
+        const classValue = classes.split(' ')[0]
+        switch(classValue) {
+            case 'product':
+                return product.image_urls.small;
+            case 'modal':
+                return product.image_urls.regular;
+            case 'results':
+                return product.image_urls.thumb
+            default:
+                return product.image_urls.regular;
+        }
+    }
+
     const hovered = isHovered ? 'hovered' : '';
 
     return (
-        <div className='img' >
-            <div className={`img__container ${classes} ${hovered}`}  onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        <div className={`img ${classes} ${hovered}`} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            {/* <div className={`img__container ${classes} ${hovered}`}  > */}
             {/* {isHovered 
                 ?   
                 <img onClick={modalOpen}  className={`img__product`}  src={product.image_urls.regular} alt={product.description}/> 
                 :    */}
-                <div className={`img__border ${classes} ${hovered}`}>
-                        <div className={`img__inset ${classes} ${hovered}`}>
-                            <img onClick={modalOpen} ref={imgRef} className={`img__product ${classes} ${hovered}`} src={product.image_urls.regular} alt={product.description}/>
-                        </div>
-                    </div>
+                {/* <div className={`img__border ${classes} ${hovered}`}> */}
+                        {/* <div className={`img__inset ${classes} ${hovered}`}> */}
+                            <img 
+                                onClick={modalOpen} 
+                                ref={imgRef} 
+                                className={`img__product ${classes} ${hovered}`} 
+                                src={imgSwitch()} 
+                                alt={product.description}/>
+                        {/* </div> */}
+                    {/* // </div> */}
             {/* } */}
-        </div>
+        {/* // </div> */}
         </div>
         
     ) 
