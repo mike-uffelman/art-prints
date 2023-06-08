0d2b2a187368fe55c21a1ddd1bc5066b58b3e59d
mike-uffelman <muffelma@gmail.com>
2023-06-07 23:23:30 -0400

    remove dotenv dependency, update gitignore

--- src/data/unsplash.js
+++ src/data/unsplash.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: `Client-ID ${UNSPLASH_KEY}`
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`
     }  
 })
