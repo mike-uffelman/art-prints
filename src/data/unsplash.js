6d8723ea88ccd5f4e72a8e1a42f7d1da3c21f9de
mike-uffelman <muffelma@gmail.com>
2023-06-07 22:58:07 -0400

    updated env variables and gitignore

--- src/data/unsplash.js
+++ src/data/unsplash.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID cYAZy8LKT_DtqjucZu-YjRh48X3vRsI4pngjYGMPuVk'
+        Authorization: `Client-ID ${UNSPLASH_KEY}`
     }  
 })
