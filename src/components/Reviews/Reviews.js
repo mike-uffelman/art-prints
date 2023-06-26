533e97cdc020f2d845f22a1f4f5fd5a7a25d243a
mike-uffelman <muffelma@gmail.com>
2023-06-25 23:51:37 -0400

    pagination navigation working as expected, fixed conditional css to display back and forward arrows depending on current page

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -7,19 +7,20 @@ import RatingStars from '../Products/Product-Reception/RatingStars';
 
 export default function Reviews({selectedReviews}) {
     const [ currentPage, setCurrentPage ] = useState(1);
-    const [ startPage, setStartPage ] = useState(0);
-    const [ startIndex, setStartIndex ] = useState(0);
-    const [ endIndex, setEndIndex] = useState(5);
+    const [ startPage, setStartPage ] = useState(1);
     const [ endPage, setEndPage ] = useState(null)
+    const [ startIndex, setStartIndex ] = useState(null);
+    const [ endIndex, setEndIndex ] = useState(null);
     const reviewsPerPage = 5;
 
     useEffect(() => {
+        console.log(currentPage)
 
         // setEndPage(selectedReviews.length/endIndex)
 
         // const count = reviews.flat(2).filter(review => review.product_id === id).length
         // setEndPage(count)
-    }, [])
+    }, [currentPage])
 
     // retrieve the product reviews from state
     // const reviews = useSelector((state) => {
@@ -27,8 +28,9 @@ export default function Reviews({selectedReviews}) {
     // })
     const { id } = useParams();
     useEffect(() => {
-        setEndIndex(Math.ceil(selectedReviews.length/reviewsPerPage))
-
+        setEndPage(Math.ceil(selectedReviews.length/reviewsPerPage))
+        setStartIndex(1);
+        setEndIndex(5)
     }, [])
 
     // if param id matches the product id render the product reviews
@@ -60,43 +62,70 @@ export default function Reviews({selectedReviews}) {
     //     )
     // })
 
+    // create and build pagination vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     let pages = []
 
-    for (let i = startIndex + 1; i <= endIndex; i++ ) {
+    for (let i = startPage; i <= endPage; i++ ) {
+        console.log('create pagination i:', i)
         pages.push(i)
     }
+
+    const renderPageButtons = pages.map((page, index) => {
+        const activePage = currentPage === index + 1 ? 'active' : '';
+
+        return (
+            <button className={`pagination__btn ${activePage}`} onClick={() => setCurrentPage(index + 1)}>{page}</button>
+        )
+    })
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     
+    const showReviews = selectedReviews.map((rev, index) => {
+        console.log(selectedReviews, index)
         
-    const showReviews = selectedReviews.map(rev => {
+        // if(index + 1 >= )
+        // if(index+1 >= startPage && index+1 <= endPage) {
+        //     return (
+        //         <div> {rev.review_id}</div>
+        //     )
+        // }
+        
         return (
-            <div> {rev.review_id}</div>
+            <div>{rev.review_id}</div>
         )
+        
     })
 
-    const renderPages = pages.map((page, index) => {
-        const activePage = currentPage === index ? 'active' : '';
-
-        return (
-            <div className={activePage} onClick={() => setCurrentPage(index)}>{page}</div>
-        )
-    })
-    
+    const handleForwardBack = (n) => {
+        console.log(currentPage, endPage, startPage)
+        setCurrentPage(currentPage + n)
+        setStartIndex(n * reviewsPerPage);
+        setEndIndex(((n + 1) * reviewsPerPage) - 1)
+    }
 
     if(!selectedReviews || selectedReviews === undefined) {
         return <div>Loading reviews...</div>
     }
 
+    const hideBack = currentPage === startPage ? 'hideBack' : '';
+    const hideForward = currentPage >= endPage ? 'hideForward' : '';
+
+
     return (
         <section className="reviews" id='reviews'>
             <h3 className='reviews__heading'>Reviews</h3>
             {/* {reviews ? renderReviews : 'Loading reviews...'} */}
+            
             {showReviews}
             <div className='review__pagination'>
                 {/* {reviewPagination} */}
-                <div>{currentPage === startPage ? '' : "<"}</div>
-                {renderPages}
-                <div>{currentPage === endPage ? '' : ">"}</div>
-
+                    <span className={`material-symbols-rounded ${hideBack}`} onClick={() => handleForwardBack(-1)}>
+                        arrow_back_ios
+                    </span>
+                    {renderPageButtons}
+                
+                    <span className={`material-symbols-rounded ${hideForward}`} onClick={() => handleForwardBack(1)}>
+                        arrow_forward_ios
+                    </span>
             </div>
         </section>
     )
