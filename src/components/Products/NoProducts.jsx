e8dae932e9e92929f60ee0044493dd48fd49b87e
mike-uffelman <muffelma@gmail.com>
2024-01-30 22:59:07 -0500

    updated results to improve fallback rendering, added styling to fallback css file

--- src/components/Products/NoProducts.jsx
+++ src/components/Products/NoProducts.jsx
@@ -1,12 +1,14 @@
 import './NoProducts.css';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function NoProducts({results}) {
+    const {term} = useParams()
 
     return (
         <React.Fragment>
             <article className='noresults'>
-                <h3 className='noresults__message'>No results found for {results ? `"${results.term}"` : 'Loading...'}</h3>
+                <h3 className='noresults__message'>No results found for {results ? `"${term}"` : 'Loading...'}</h3>
 
             </article>
         </React.Fragment>
--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -5,7 +5,6 @@ import { useSelector } from "react-redux";
 import Pagination from "../components/Products/Pagination/Pagination";
 import NoProducts from "../components/Products/NoProducts";
 
-
 // layout component for the search results page
 export default function ResultsPage() {
     // get the search data from store
@@ -13,30 +12,42 @@ export default function ResultsPage() {
         return state.search
     })
 
+
     // filter tags 
     const tagsData = Object.entries(results.tags).filter(tags => tags[1] > 0).map(tag => tag[0])
 
     if(!results || results === undefined) {
         return <div>Loading...</div>
     }
-
-    if(results.results.flat().length === 0) {
+    
+    if (results.results.flat().length === 0) {
         return <NoProducts results={results}/>
-    } else {
+    } else if (results.results.flat().length > 0) {
         return (
             <React.Fragment >
 
                 {/* check for tags and results, render components if defined, otherwise render a loading message */}
-                {tagsData ? <Tags tagsData={tagsData}/> : 'Loading tags...'}
-                {results.results ? <Products className='products__container' results={results.results}/> : 'Loading photos...'}
+                {tagsData ? <Tags tagsData={tagsData}/> : 'Loading Tags...'}
+                {results.results.flat().length > 0 ? <Products className='products__container' results={results.results}/> : 'Loading photos...'}
 
                 {/* Render pagination button below content */}
                 <Pagination />
 
             </React.Fragment>
         
-    )
-    }
+        )
+    } 
+
+    // if(isLoading && results.results.flat().length === 0) {
+    //     console.log('Loading: ', isLoading, results.results.flat().length, results)
+
+    //     return (
+    //         <div className='loading'>Looking for &quot;{term}&quot;</div>
+    //     )
+    // } else 
+    
+    
+    
 
     
 }
